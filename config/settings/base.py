588389166aa7e505328397a8f7b9af6c421e769d
webmalc <m@webmalc.pw>
2026-02-08 16:06:04 +0300

    Update tinymce

--- config/settings/base.py
+++ config/settings/base.py
@@ -204,6 +204,20 @@ FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
+# TinyMCE
+# ------------------------------------------------------------------------------
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "500px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor "
+    "searchreplace visualblocks code fullscreen insertdatetime media "
+    "table paste code help wordcount",
+    "toolbar": "undo redo | formatselect | bold italic backcolor | "
+    "alignleft aligncenter alignright alignjustify | "
+    "bullist numlist outdent indent | removeformat | code | help",
+    "custom_undo_redo_levels": 10,
+}
+
 # FIXTURES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#fixture-dirs
