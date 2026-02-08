3f623eea3c3c44a08107f4ab306733788dcde558
webmalc <m@webmalc.pw>
2026-02-08 13:46:33 +0300

    Add content model

--- mjk/content/models.py
+++ mjk/content/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Content(models.Model):
+    slug = models.SlugField(
+        db_index=True,
+    )
+    content = models.TextField()
+    description = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    keywords = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    title = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return self.slug
--- mjk/content/views.py
+++ mjk/content/views.py
@@ -4,11 +4,6 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "content/index.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["style_version"] = self.request.GET.get("style_version", "")
-        return context
-
 
 class AboutView(TemplateView):
     template_name = "content/about.html"
